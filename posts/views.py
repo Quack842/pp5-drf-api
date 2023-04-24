972e9d52ca890940d09d2cfc730152b0c8f3c04c
Nicolene Marais <nici.marais.ie@gmail.com>
2023-04-24 08:22:43 +0000

    Fixed spelling error on posts > views.py Line 1

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.sb.models import Count
+from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
