48b92c57d154d63b62a4dbc0a85bbdab054b68bd
Nicolene Marais <nici.marais.ie@gmail.com>
2023-04-27 06:55:03 +0000

    changes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 import re
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
