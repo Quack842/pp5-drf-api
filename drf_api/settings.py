292f0598840a59c4d5a91082f1177f6be6ac3e5f
Quack842 <>
2023-05-17 13:09:12 +0000

    Changed validator line 173 settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -170,7 +170,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.validate_password",
+        "NAME": "registration.validators.validate_password",
     },
 ]
 
