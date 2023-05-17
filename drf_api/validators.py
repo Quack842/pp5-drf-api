3caa10ba83b1e47bbd3babb88696c269748ef6f9
Quack842 <>
2023-05-17 13:03:45 +0000

    Edited Validation code in validators.py and changed accrodingly in settings.py line 173

--- drf_api/validators.py
+++ drf_api/validators.py
@@ -2,19 +2,15 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
 
-class CustomPasswordValidator():
+def validate_password(value):
+    # Check if the password contains at least one number
+    if not any(char.isdigit() for char in value):
+        raise ValidationError(_("The password must contain at least one number."), code='no_number')
 
-    def __init__(self, min_length=1):
-        self.min_length = min_length
+    # Check if the password contains at least one special character
+    if not any(char in '!@#$%^&*()_+-=[]{}|\\:;"\'<>,.?/~`' for char in value):
+        raise ValidationError(_("The password must contain at least one special character."), code='no_special_char')
 
-    def validate(self, password, user=None):
-        special_characters = "[~\!@#\$%\^&\*\(\)_\+{}\":;'\[\]]"
-        if not any(char.isdigit() for char in password):
-            raise ValidationError(_('Password must contain at least %(min_length)d digit.') % {'min_length': self.min_length})
-        if not any(char.isalpha() for char in password):
-            raise ValidationError(_('Password must contain at least %(min_length)d letter.') % {'min_length': self.min_length})
-        if not any(char in special_characters for char in password):
-            raise ValidationError(_('Password must contain at least %(min_length)d special character.') % {'min_length': self.min_length})
-
-    def get_help_text(self):
-        return ""
+    # Check if the password contains at least one capital letter
+    if not any(char.isupper() for char in value):
+        raise ValidationError(_("The password must contain at least one capital letter."), code='no_capital_letter')
